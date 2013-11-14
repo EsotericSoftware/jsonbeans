1c65622589842e5d22d7cdb24b44bb49f913607b
Alexander Kriegisch <kriegisch@atis-systems.com>
2013-11-14 12:43:18 +0100

    Move folders to standard Maven layout:
    src
      main
        java
      test
        java
    
    The test folder does not exist though (no tests available).

--- src/com/esotericsoftware/jsonbeans/JsonSerializable.java
+++
@@ -1,8 +0,0 @@
-
-package com.esotericsoftware.jsonbeans;
-
-public interface JsonSerializable {
-	public void write (Json json);
-
-	public void read (Json json, JsonValue jsonData);
-}
--- src/com/esotericsoftware/jsonbeans/JsonSerializer.java
+++
@@ -1,8 +0,0 @@
-
-package com.esotericsoftware.jsonbeans;
-
-public interface JsonSerializer<T> {
-	public void write (Json json, T object, Class knownType);
-
-	public T read (Json json, JsonValue jsonData, Class type);
-}
