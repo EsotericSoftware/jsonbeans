c804f089b73afa77b734b62059d3b250e01fc305
Jesus Zazueta <jjzazuet@vacco.io>
2021-04-12 07:48:51 -0400

    - Removing unnecessary build statements.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,4 @@ version = "1.0.0"
 
 configure<io.vacco.oss.gitflow.GsPluginProfileExtension> {
   sharedLibrary(true, false)
-  addClasspathHell()
-}
-
-configure<io.vacco.cphell.ChPluginExtension> {
-  resourceExclusions.add("module-info.class")
 }
