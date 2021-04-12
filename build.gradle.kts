91ed63c554980f1f828b48097f5549d3a9b1ef9b
Jesus Zazueta <jjzazuet@vacco.io>
2021-04-12 08:18:54 -0400

    - Setting Java8 as runtime baseline.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,8 @@ version = "1.0.0"
 configure<io.vacco.oss.gitflow.GsPluginProfileExtension> {
   sharedLibrary(true, false)
 }
+
+configure<JavaPluginExtension> {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
